8b358a917fc5e6fd76416551508b4894d94b0443
Yousaf7860-dev <yousafansar7860@gmail.com>
2025-07-14 15:43:45 +0500

    first commit from dev2
    # Conflicts:
    #	.idea/vcs.xml

---
+++ app/src/main/java/com/example/firstxmlprojectainotepad/presentation/features/setting_activity/SettingsActivity.kt
@@ -0,0 +1,22 @@
+package com.example.firstxmlprojectainotepad.presentation.features.setting_activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firstxmlprojectainotepad.R
+import com.example.firstxmlprojectainotepad.databinding.ActivitySettingsBinding
+
+class SettingsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        enableEdgeToEdge()
+
+
+
+    }
+}
