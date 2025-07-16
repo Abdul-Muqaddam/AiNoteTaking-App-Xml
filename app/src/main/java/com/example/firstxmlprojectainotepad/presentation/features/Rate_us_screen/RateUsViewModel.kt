a383e34eb2c70781266f48fd2a7de8308471cd6a
Abdul-Muqaddam <chandansar444999@gmail.com>
2025-07-16 10:42:02 +0500

    updates

---
+++ app/src/main/java/com/example/firstxmlprojectainotepad/presentation/features/Rate_us_screen/RateUsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.firstxmlprojectainotepad.presentation.features.Rate_us_screen
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+data class RateUsViewModelState(
+    val reviewText:Int,
+    val reviewImg:Int,
+    val selectedStart:Int
+)
+
+
